6aae8ebdafc1c1c36b12a701f48564aea1def745
kusano <kusano@users.noreply.github.com>
2019-03-19 20:04:15 +0900

    CNN版

--- avgface.py
+++ avgface.py
@@ -40,23 +40,57 @@ valid_id = image_id[-2000:-1000]
 test_id = image_id[-1000:]
 
 # モデル
-def encoder(x):
+def encoder(x, training):
   with tf.variable_scope("encoder", reuse=tf.AUTO_REUSE):
-    h = tf.reshape(x, [-1, img_w*img_h*3])
-    h = tf.layers.dense(h, 1024, tf.nn.relu)
-    h = tf.layers.dense(h, 1024, tf.nn.relu)
+    h = x
+    # (40, 40, 3)
+    h = tf.layers.conv2d(h, 64, (5, 5), (2, 2), padding="same")
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (20, 20, 64)
+    h = tf.layers.conv2d(h, 128, (5, 5), (2, 2), padding="same")
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (10, 10, 128)
+    h = tf.layers.conv2d(h, 256, (5, 5), (2, 2), padding="same")
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (5, 5, 256)
+    h = tf.layers.flatten(h)
+    # (6400)
+    h = tf.layers.dense(h, 1024)
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (1024)
     mean = tf.layers.dense(h, 1024)
     var = tf.layers.dense(h, z_dim, tf.nn.softplus)
-    return mean, var
+  return mean, var
 
-def decoder(z):
+def decoder(z, training):
   with tf.variable_scope("decoder", reuse=tf.AUTO_REUSE):
     h = z
-    h = tf.layers.dense(h, 1024, tf.nn.relu)
-    h = tf.layers.dense(h, 1024, tf.nn.relu)
-    h = tf.layers.dense(h, img_w*img_h*3, tf.nn.sigmoid)
-    y = tf.reshape(h, [-1, img_w, img_h, 3])
-    return y
+    # (1024)
+    h = tf.layers.dense(h, 6400)
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (6400)
+    h = tf.reshape(h, [-1, 5, 5, 256])
+    # (5, 5, 256)
+    h = tf.layers.conv2d_transpose(h, 256, (3, 3), (2, 2), padding="same")
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (10, 10, 128)
+    h = tf.layers.conv2d_transpose(h, 64, (3, 3), (2, 2), padding="same")
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (20, 20, 64)
+    h = tf.layers.conv2d_transpose(h, 32, (3, 3), (2, 2), padding="same")
+    h = tf.layers.batch_normalization(h, training=training)
+    h = tf.nn.relu(h)
+    # (40, 40, 32)
+    h = tf.layers.conv2d(h, 3, (3, 3), padding="same", activation=tf.nn.sigmoid)
+    # (40, 40, 3)
+  return h
 
 sess = tf.Session()
 
@@ -66,22 +100,23 @@ def train():
     return tf.log(tf.maximum(x, 1e-10))
 
   x = tf.placeholder(tf.float32, [None, img_w, img_h, 3])
-  mean, var = encoder(x)
+  mean, var = encoder(x, True)
   z = mean + tf.sqrt(var) * tf.random_normal(tf.shape(mean))
-  y = decoder(z)
+  y = decoder(z, True)
 
   KL = -0.5*tf.reduce_mean(tf.reduce_sum(1+tf_log(var)-mean**2-var, axis=1))
   entropy = tf.reduce_mean(tf.reduce_sum(x*tf_log(y) + (1-x)*tf_log(1-y), axis=[1, 2, 3]))
   lower_bound = [-KL, entropy]
   cost = -tf.reduce_sum(lower_bound)
   optimize = tf.train.AdamOptimizer().minimize(cost)
+  update = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 
   sess.run(tf.global_variables_initializer())
 
   batch_size = 1000
-  for idx in range(0, 10000, batch_size):
+  for idx in range(0, len(train_id), batch_size):
     img = [read_image(id) for id in train_id[idx:idx+batch_size]]
-    _, lb = sess.run([optimize, lower_bound], feed_dict={x: img})
+    _, _, lb = sess.run([optimize, update, lower_bound], feed_dict={x: img})
     print(idx, lb)
 
 train()
@@ -89,21 +124,21 @@ train()
 # 潜在変数の平均値を求める
 x = tf.placeholder(tf.float32, [None, img_w, img_h, 3])
 img = [read_image(id) for id in valid_id]
-z, _ = sess.run(encoder(x), feed_dict={x: img})
+z, _ = sess.run(encoder(x, False), feed_dict={x: img})
 z_avg = np.mean(z, axis=0)
 print("z_avg", z_avg.tolist())
 
 # 顔を平均顔に近づける
 num = 16
 org = [read_image(id) for id in test_id[:num]]
-z_in, _ = sess.run(encoder(x), feed_dict={x: org})
+z_in, _ = sess.run(encoder(x, False), feed_dict={x: org})
 
 z = tf.placeholder(tf.float32, [None, z_dim])
 
 result = [[None]*11 for _ in range(num)]
 for i in range(0, 11):
   p = i/10  # [0.0, 1.0]
-  res = sess.run(decoder(z), feed_dict={z: z_in*(1-p)+z_avg*p})
+  res = sess.run(decoder(z, False), feed_dict={z: z_in*(1-p)+z_avg*p})
   for j in range(num):
     result[j][i] = res[j]
 
